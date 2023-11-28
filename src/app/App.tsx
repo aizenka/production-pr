e4efa7dbe894e5959bcb88607080a35dd2c2a306
Andrew <jadedynastymail.ru@mail.ru>
2023-11-28 18:34:41 +0300

    update themes, loki tests

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
-import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from 'app/providers/Router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
-import { classNames } from 'shared/lib'
 import './styles/index.scss'
 
 const App = () => {
-  const { theme } = useTheme()
 
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className='app'>
       <Navbar />
       <div className='contentLayout'>
         <Sidebar />
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo, useState } from 'react'
+import { FC, useMemo, useEffect, useState } from 'react'
 import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from '../lib/ThemeContext'
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
@@ -9,6 +9,14 @@ interface ThemeProviderProps {
 
 const ThemeProvider: FC <ThemeProviderProps> = ({ initialTheme, children }) => {
   const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme)
+
+  useEffect(() => {
+    document.body.classList.add(theme)
+
+    return () => {
+      document.body.classList.remove(theme)
+    }
+  }, [theme])
 
   const defaultProviderProps = useMemo(() => {
     return {
