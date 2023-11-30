cbb84407fe6785d3fffac644941ece446143f154
Andrew <jadedynastymail.ru@mail.ru>
2023-11-30 14:50:14 +0300

    add husky

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
-import { classNames } from 'shared/lib'
 import { Modal } from 'shared/ui'
 import { LoginForm } from '../LoginForm/LoginForm'
-import cls from './LoginModal.module.scss'
+
 
 interface LoginModalProps {
   className?: string,
@@ -11,14 +10,12 @@ interface LoginModalProps {
 
 export const LoginModal = (props: LoginModalProps) => {
   const {
-    className,
     isOpen,
     onClose
   } = props
 
   return (
     <Modal
-      className={classNames(cls.loginModal, {}, [className])}
       open={isOpen}
       onClose={onClose}
       lazy
