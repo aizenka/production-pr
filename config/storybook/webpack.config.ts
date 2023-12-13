429b5940b4eb283df01d55b7ebaba62fe6df81ef
Andrew <jadedynastymail.ru@mail.ru>
2023-12-13 20:59:22 +0300

    fix storybook wepback DefinePlugin api variable value

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -33,7 +33,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
   config!.plugins!.push(
     new DefinePlugin({
       __IS_DEV__: true,
-      __API_URL__: ''
+      __API_URL__: true
     })
   )
 
