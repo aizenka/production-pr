0f7c8cd331b4622c5700c26b5c5a27da841ff9b6
Andrew <jadedynastymail.ru@mail.ru>
2024-01-11 19:31:09 +0300

    fix build

--- src/features/AddCommentForm/model/slice/addCommentFormSlice.ts
+++ src/features/AddCommentForm/model/slice/addCommentFormSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { AddCommentFormSchema } from '../types/addCommentForm'
+import { AddCommentFormSchema } from '../types/AddCommentForm'
 
 const initialState: AddCommentFormSchema = {
   text: ''
