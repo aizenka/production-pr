c865efde617698128c037f665f7ab1199fd508c7
Andrew <jadedynastymail.ru@mail.ru>
2024-01-24 17:22:06 +0300

    fix fetchArticleListNextPage test

--- src/pages/ArticlesPage/model/services/fetchArticleListNextPage/fetchArticleListNextPage.test.ts
+++ src/pages/ArticlesPage/model/services/fetchArticleListNextPage/fetchArticleListNextPage.test.ts
@@ -1,9 +1,8 @@
 import { testAsyncThunk } from 'shared/lib/tests'
-// import { fetchArticleList } from '../fetchArticleList/fetchArticleList'
+import { fetchArticleList } from '../fetchArticleList/fetchArticleList'
 import { fetchArticleListNextPage } from './fetchArticleListNextPage'
 
-// manual mock not working
-// jest.mock('../fetchArticleList/fetchArticleList')
+jest.mock('../fetchArticleList/fetchArticleList')
 
 describe('fetchArticleListNextPageTest', () => {
   test('sucess fetch next page', async () => {
@@ -17,32 +16,34 @@ describe('fetchArticleListNextPageTest', () => {
       hasMore: true
     }
 
-    const { callThunk, api, dispatch } = testAsyncThunk(fetchArticleListNextPage)
-    api.get.mockReturnValue(Promise.resolve({ data: initialState }))
+    const { callThunk, dispatch } = testAsyncThunk(fetchArticleListNextPage, {
+      articlesPage: initialState
+    })
 
     await callThunk()
 
-    expect(dispatch).toBeCalledTimes(2) // 4 after fix mock
-    // expect(fetchArticleList).toHaveBeenCalledWith({ page: 3 }) // after fix mock
+    expect(dispatch).toBeCalledTimes(4)
+    expect(fetchArticleList).toHaveBeenCalledWith({ page: 3 })
   })
 
-  // test('fetchArticleList not called', async () => {
+  test('fetchArticleList not called', async () => {
 
-  //   const initialState = {
-  //     page: 2,
-  //     ids: [],
-  //     entities: {},
-  //     limit: 4,
-  //     isLoading: false,
-  //     hasMore: false
-  //   }
+    const initialState = {
+      page: 2,
+      ids: [],
+      entities: {},
+      limit: 4,
+      isLoading: false,
+      hasMore: false
+    }
 
-  //   const { callThunk, api, dispatch } = testAsyncThunk(fetchArticleListNextPage)
-  //   api.get.mockReturnValue(Promise.resolve({ data: initialState }))
+    const { callThunk, dispatch } = testAsyncThunk(fetchArticleListNextPage, {
+      articlesPage: initialState
+    })
 
-  //   await callThunk()
+    await callThunk()
 
-  //   expect(dispatch).toBeCalledTimes(2)
-  //   // expect(fetchArticleList).not.toHaveBeenCalled() // after fix mock
-  // })
+    expect(dispatch).toBeCalledTimes(2)
+    expect(fetchArticleList).not.toHaveBeenCalled()
+  })
 })
